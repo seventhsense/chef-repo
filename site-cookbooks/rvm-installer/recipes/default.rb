1414b4ec52aef465b30fc3fc5642a94bf6b6d5a1
seventhsense <seventh@lh4l.com>
2014-02-01 23:58:26 +0900

    add .railsrc

--- site-cookbooks/rvm-installer/recipes/default.rb
+++ site-cookbooks/rvm-installer/recipes/default.rb
@@ -6,14 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-rvm_ruby "ruby-2.0.0" do
-  action :install
+# rvm_ruby "ruby-2.0.0" do
+  # action :install
+# end
+
+# rvm_default_ruby "ruby-2.0.0"
+
+# rvm_gem 'rails' do
+  # ruby_string "ruby-2.0.0"
+  # version '4.0.2'
+  # action :install
+# end
+template "/home/vagrant/.railsrc" do
+  source ".railsrc"
+  owner "vagrant"
+  group "vagrant"
 end
-
-rvm_default_ruby "ruby-2.0.0"
-
-rvm_gem 'rails' do
-  ruby_string "ruby-2.0.0"
-  version '4.0.2'
-  action :install
+template "/home/vagrant/.template.rb" do
+  source ".template.rb"
+  owner "vagrant"
+  group "vagrant"
 end
---
+++ site-cookbooks/rvm-installer/templates/default/.template.rb
@@ -0,0 +1,6 @@
+puts "git initial commit"
+git :init
+git :add => '.'
+git :commit => "-m 'initial commit'"
+
+puts "done."
